b78747c6d5548b2acd8eadb90fd27ffeeca9589e
ashu-rb <78752052+ashu-rb@users.noreply.github.com>
2022-10-16 11:05:42 +0530

    Added to files

---
+++ js/store-data.js
@@ -0,0 +1,718 @@
+let stores = [{
+    "recommendation": {},
+    "storeNumber": "5758-13907",
+    "id": "15051",
+    "name": "Pet Food Store Care",
+    "phoneNumber": "+91 9013157434",
+    "coordinates": {
+        "latitude": 27.2046,
+        "longitude": 77.4977
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "Chandni Chowk,New Delhi",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Chandni Chowk",
+        "countrySubdivisionCode": "New Delhi",
+        "countryCode": "India",
+        "postalCode": "110006"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "Pacific Standard Time",
+        "olsonTimeZoneId": "GMT-08:00 America/Los_Angeles"
+    },
+    "brandName": "Starbucks",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 8:00 PM",
+    "addressLines": ["Chandni Chowk,New Delhi", "India 110006"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "GO",
+        "name": "Google Wi-Fi"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "PS",
+        "name": "Playbook Store System"
+    }, {
+        "code": "WC",
+        "name": "Wireless Charging"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "la-cienega-gregory-way-257-s-la-cienega-blvd-beverly-hills-ca-902113301-u"
+}, {
+    "recommendation": {},
+    "storeNumber": "5577-1851",
+    "id": "14753",
+    "name": "Pets Food",
+    "phoneNumber": "(999) 888-7845",
+    "coordinates": {
+        "latitude": 28.575950,
+        "longitude": 77.227348
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "6066 West Olympic Boulevard",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Los Angeles",
+        "countrySubdivisionCode": "CA",
+        "countryCode": "US",
+        "postalCode": "900364402"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "Pacific Standard Time",
+        "olsonTimeZoneId": "GMT-08:00 America/Los_Angeles"
+    },
+    "brandName": "Starbucks",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 12:00 PM",
+    "addressLines": ["Saket", "New Delhi, India 110036"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "GO",
+        "name": "Google Wi-Fi"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "PS",
+        "name": "Playbook Store System"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "fairfax-olympic-6066-west-olympic-boulevard-los-angeles-ca-9003644"
+}, {
+    "recommendation": {},
+    "storeNumber": "16279-169147",
+    "id": "114059",
+    "name": "Royals Pet Food",
+    "phoneNumber": "888-990-1234",
+    "coordinates": {
+        "latitude": 32.494994,
+        "longitude": 76.266811
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "8480 Beverly Blvd",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Los Angeles",
+        "countrySubdivisionCode": "CA",
+        "countryCode": "US",
+        "postalCode": "900483414"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "Pacific Standard Time",
+        "olsonTimeZoneId": "GMT-08:00 America/Los_Angeles"
+    },
+    "brandName": "Starbucks",
+    "ownershipTypeCode": "LS",
+    "open": true,
+    "openStatusText": "Open 24x7",
+    "addressLines": ["Jammu & Kahsmir", "India, 678902"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "8:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "8:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "8:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "8:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "8:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "8:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "8:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "WF",
+        "name": "Wireless Hotspot"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "RW",
+        "name": "My Starbucks Rewards"
+    }],
+    "slug": "target-la-beverly-t-2775-8480-beverly-blvd-los-angeles-ca-900483414-us"
+}, {
+    "recommendation": {},
+    "storeNumber": "75244-90562",
+    "id": "9620",
+    "name": "UNIQ Pet Food",
+    "phoneNumber": "910-423-6260",
+    "coordinates": {
+        "latitude": 19.10257703181017,
+        "longitude":  72.87833361342304
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "UNIQ Pet Food",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Los Angeles",
+        "countrySubdivisionCode": "CA",
+        "countryCode": "US",
+        "postalCode": "900481804"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "Pacific Standard Time",
+        "olsonTimeZoneId": "GMT-08:00 America/Los_Angeles"
+    },
+    "brandName": "Starbucks",
+    "ownershipTypeCode": "LS",
+    "open": true,
+    "openStatusText": "Open until 10:00 PM",
+    "addressLines": ["Bandra", "Mumbai, 190048"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 5:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 3:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "4:00 AM to 10:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "4:00 AM to 10:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "4:00 AM to 10:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "4:00 AM to 10:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "4:00 AM to 10:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "BA",
+        "name": "Bakery"
+    }],
+    "slug": "cedars-sinai-medical-center-8700-beverly-blvd-los-angeles-ca-900481804-us"
+}, {
+    "recommendation": {},
+    "storeNumber": "12900-154604",
+    "id": "19370",
+    "name": "Pet Food Dealers",
+    "phoneNumber": "919-931-1013",
+    "coordinates": {
+        "latitude": 12.952509498539179,
+        "longitude": 77.5718312940069
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "South Lane,Highway",
+        "streetAddressLine2": "#106",
+        "streetAddressLine3": null,
+        "city": "Los Angeles",
+        "countrySubdivisionCode": "CA",
+        "countryCode": "US",
+        "postalCode": "900365810"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "Pacific Standard Time",
+        "olsonTimeZoneId": "GMT-08:00 America/Los_Angeles"
+    },
+    "brandName": "Starbucks",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["Pet Food Dealers", "Main Highway", "Banglore, India"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "CL",
+        "name": "Starbucks Reserve-Clover Brewed"
+    }, {
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "GO",
+        "name": "Google Wi-Fi"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "WC",
+        "name": "Wireless Charging"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "wilshire-curson-5757-wilshire-blvd-los-angeles-ca-900365810-us"
+}, {
+    "recommendation": {},
+    "storeNumber": "22415-184968",
+    "id": "1006844",
+    "name": "Petify Foods Corner",
+    "phoneNumber": "999-123-9360",
+    "coordinates": {
+        "latitude": 25.55132048168851,
+        "longitude": 91.20614441401551
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "Petify Foods Corner",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Los Angeles",
+        "countrySubdivisionCode": "CA",
+        "countryCode": "US",
+        "postalCode": "90036"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "Pacific Standard Time",
+        "olsonTimeZoneId": "GMT-08:00 America/Los_Angeles"
+    },
+    "brandName": "Starbucks",
+    "ownershipTypeCode": "LS",
+    "open": true,
+    "openStatusText": "Open until 11:00 PM",
+    "addressLines": ["Main City,Ghuwhati", "Dispur,India"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "5:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "5:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "5:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "5:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "5:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "5:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "5:00 AM to 9:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "DR",
+        "name": "Digital Rewards"
+    }],
+    "slug": "ralphs-los-angeles-289-5601-wilshire-blvd-los-angeles-ca-90036-us"
+}, {
+    "recommendation": {},
+    "storeNumber": "5871-30359",
+    "id": "15639",
+    "name": "Food & Care PetHome",
+    "phoneNumber": "901-131-6597",
+    "coordinates": {
+        "latitude": 22.728881091105094, 
+        "longitude": 75.87964274565691
+    },
+    "regulations": [],
+    "address": {
+        "streetAddressLine1": "164 N. Robertson",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "West Hollywood",
+        "countrySubdivisionCode": "CA",
+        "countryCode": "US",
+        "postalCode": "900483109"
+    },
+    "timeZoneInfo": {
+        "currentTimeOffset": -420,
+        "windowsTimeZoneId": "Pacific Standard Time",
+        "olsonTimeZoneId": "GMT-08:00 America/Los_Angeles"
+    },
+    "brandName": "Starbucks",
+    "ownershipTypeCode": "CO",
+    "open": true,
+    "openStatusText": "Open until 9:00 PM",
+    "addressLines": ["Tower Mall,Main City", "Indore, MP 400525"],
+    "mop": {
+        "ready": false,
+        "wait": null
+    },
+    "schedule": [{
+        "dayName": "Today",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tomorrow",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Monday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Tuesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Wednesday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Thursday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }, {
+        "dayName": "Friday",
+        "hours": "6:00 AM to 6:00 PM",
+        "open": true,
+        "holiday": ""
+    }],
+    "features": [{
+        "code": "WA",
+        "name": "Oven-warmed Food"
+    }, {
+        "code": "WF",
+        "name": "Wireless Hotspot"
+    }, {
+        "code": "VS",
+        "name": "Verismo"
+    }, {
+        "code": "CD",
+        "name": "Mobile Payment"
+    }, {
+        "code": "DR",
+        "name": "Digital Rewards"
+    }, {
+        "code": "LB",
+        "name": "LaBoulange"
+    }, {
+        "code": "XO",
+        "name": "Mobile Order and Pay"
+    }, {
+        "code": "MX",
+        "name": "Music Experience"
+    }, {
+        "code": "NB",
+        "name": "Nitro Cold Brew"
+    }, {
+        "code": "BE",
+        "name": "Blonde Espresso"
+    }, {
+        "code": "LU",
+        "name": "Lunch"
+    }, {
+        "code": "PS",
+        "name": "Playbook Store System"
+    }, {
+        "code": "WC",
+        "name": "Wireless Charging"
+    }, {
+        "code": "UE",
+        "name": "tbd - Uber Eats"
+    }],
+    "slug": "beverly-robertson-164-n-robertson-west-hollywood-ca-900483109-us"
+}]
